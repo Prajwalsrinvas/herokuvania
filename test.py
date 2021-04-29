0061bc0c024bf3398bb153a8668735cde70448b2
Prajwal Srinivas <24704548+Prajwalsrinvas@users.noreply.github.com>
2021-04-29 14:05:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ def coursevania_scraper(course_count):
     chrome_options.add_argument('--no-sandbox')
     chrome_options.binary_location = GOOGLE_CHROME_PATH
     chrome_options.headless = True
-    driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     driver.get(url)
     count = 0
 
